ceb0502553669dde5ad6d788677f395f19aeb4e4
scullycmjmu <54854695+scullycmjmu@users.noreply.github.com>
2020-03-17 13:43:18 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Shoeinfo from './Shoeinfo';
+
+class App extends Component {
+  state = {
+    moreShoes:[
+      {brand:"Adidas", type:"Zoom", size:13},
+    ]
+  }
+  changeShoe = (updatedType) => {
+    this.setState({
+      moreShoes:[
+        {brand:"Puma",type: "Bolt", size:14},
+      ]
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Christian Scully</h1>
+        <Shoeinfo brand="Nike" type="Lebron 9" size="13"/>
+        <Shoeinfo brand="Jordan" type="Zion 1" size="11"/>
+
+        <h1>I have many shoes!</h1>
+        <Shoeinfo brand={this.state.moreShoes[0].brand} type={this.state.moreShoes[0].type} size={this.state.moreShoes[0].size}/>
+        <button onClick={this.changeShoe}>Click to see hidden shoe</button>
+      </div>
+
+    );
+  }
 }
 
 export default App;
+
