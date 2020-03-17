39db3dcf74aaa231e58c37b748ea39d67bf5788f
scullycmjmu <christianscully@Christians-Air.hsd1.nj.comcast.net>
2020-03-17 11:40:09 -0400

    Updates

---
+++ precache-manifest.3b2f72e834f8a275789ed22ce5979f66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09fbc65280554bf1852310a16b76ea52",
+    "url": "/scullycmjmu/my-app.git/index.html"
+  },
+  {
+    "revision": "44475622f3cfd6f12a7b",
+    "url": "/scullycmjmu/my-app.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "66c4d723d19fe3ae70b8",
+    "url": "/scullycmjmu/my-app.git/static/js/2.2377b599.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/scullycmjmu/my-app.git/static/js/2.2377b599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44475622f3cfd6f12a7b",
+    "url": "/scullycmjmu/my-app.git/static/js/main.a36a76c6.chunk.js"
+  },
+  {
+    "revision": "0604a29551bb427c7d2a",
+    "url": "/scullycmjmu/my-app.git/static/js/runtime-main.377f6166.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scullycmjmu/my-app.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
